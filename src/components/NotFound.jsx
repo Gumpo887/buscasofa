ea81e8b3c7ba1412e45e930902eacd60f6ca6ed0
Jesús Martínez Alonso <7991jm@gmail.com>
2026-06-06 19:13:42 +0200

    feat(pagina-no-encontrada): añade pagina fallback para rutas desconocidas

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound() {
+    return (
+        <main>
+            <h1>No hemos encontrado la página que buscas</h1>
+        </main>
+    );
+}
+
+export default NotFound;
